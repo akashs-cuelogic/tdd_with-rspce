3bbc6bb7b802cbdc31d485585d4984dfab775e6d
Akash Shah <akash.shah@cuelogic.co.in>
2015-07-13 16:04:43 +0530

    after rubocop and testing overcommit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
     username 'john_doe'
-    #sequence(:username) { |n| 'john_doe#{n}' }
+    # sequence(:username) { |n| 'john_doe#{n}' }
     sequence(:email) { |n| 'john_doe#{n}@example.com'  }
     status 1
-    
   end
-
 end
